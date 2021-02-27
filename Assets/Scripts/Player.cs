a18a75664a4b7ff8fb39af2258398919ca84faaf
pablovass <faktory.fm@live.com>
2021-02-27 15:11:24 -0300

    create the aim of player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,9 +10,10 @@ public class Player : MonoBehaviour
     float h;
     float v;
     private Vector3 moveDirection;
-    [SerializeField]float speed = 6; 
-    
-    
+    [SerializeField]float speed = 10;
+    [SerializeField] Transform aim;
+    [SerializeField]  Camera camera; 
+    Vector2 facingDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +29,8 @@ public class Player : MonoBehaviour
         moveDirection.y = v;
         
         transform.position += moveDirection * Time.deltaTime * speed; 
-
+        //la mira
+        facingDirection= camera.ScreenToWorldPoint(Input.mousePosition)- transform.position;
+        aim.position = transform.position + (Vector3)facingDirection.normalized;
     }
 }
